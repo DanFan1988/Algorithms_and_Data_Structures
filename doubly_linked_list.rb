c446075c321c89a14378eb24d05bdd8dc06d8c72
Dan Fan <dan.fan1988@gmail.com>
2014-03-01 01:25:35 -0800

    doubly linked list implimented. yay

--- doubly_linked_list.rb
+++ doubly_linked_list.rb
@@ -8,16 +8,27 @@ class Link
 end
 
 class DoublyLinkedList
-	attr_accessor :head, :tail :length
+	attr_accessor :head, :tail
 
 	def initialize(*values)
 		@head = nil
 		@tail = nil
 
-		@length = values.length
 		values.each do |value|
 			self.push(value)
 		end
+	end
+
+	def [](idx)
+		#can handle negative indices
+		if idx < 0
+			here = @tail
+			((idx.abs)-1).times { here = here.prev }
+		else
+			here = @head
+			idx.times { here = here.next }
+		end
+		here
 	end
 
 	def empty?
@@ -32,29 +43,60 @@ class DoublyLinkedList
 		end
 	end
 
+	def unshift(value)
+		if self.empty?
+			@head = @tail = Link.new(value)
+		else
+			self.insert_before(@head, value)
+		end
+	end
+
 	def insert_after(link, value)
 		raise "cannot insert after a non-Link Object" unless link.is_a?(Link)
 
 		new_link = Link.new(value)
+
 		if link.next.nil?
-			new_link.prev = link
-			link.next = new_link
+			@tail = new_link
 		else
 			link_after = link.next
-
 			link_after.prev = new_link
 			new_link.next = link_after
-
-			link.next = new_link
-			new_link.prev = link
 		end
+
+		link.next = new_link
+		new_link.prev = link
 	end
 
+	def insert_before(link, value)
+		raise "cannot insert before a non-Link Object" unless link.is_a?(Link)
 
+		new_link = Link.new(value)
 
+		if link.prev.nil?
+			@head = new_link
+		else
+			link_before = link.prev
+			link_before.next = new_link
+			new_link.prev = link_before
+		end
+
+		link.prev = new_link
+		new_link.next = link
+	end
+
+	def remove(link)
+		link.prev.next = link.next if link.prev
+		link.next.prev = link.prev if link.next
+
+		@head = link.next if link.prev.nil?
+		@tail = link.prev if link.next.nil?
+	end
 end
 
-a = DoublyLinkedList.new
+a = DoublyLinkedList.new(1,2,3)
 
-puts a.length, a.head
+puts a.head.value, a.tail.value
+puts a.remove(a[0])
+p a.head.prev
 
