00ef26981661753faee31271ef76043d2bc03071
Dan Fan <dan.fan1988@gmail.com>
2014-03-01 05:54:48 -0800

    queue done

--- queue.rb
+++ queue.rb
@@ -1,2 +1,25 @@
 class Queue
+	attr_reader :queue
+
+	alias_method :size, :length
+
+	def initialize(queue = [])
+		@queue = queue
+	end
+
+	def enqueue(item)
+		@queue.push(item)
+	end
+
+	def dequeue
+		@queue.shift
+	end
+
+	def length
+		@queue.length
+	end
+
+	def empty?
+		@queue.empty?
+	end
 end
