c10a660fb01a2050161d63acca83b3a47d269819
Dan Fan <dan.fan1988@gmail.com>
2014-03-01 18:40:46 -0800

    tree node implimented

---
+++ tree_node.rb
@@ -0,0 +1,35 @@
+class TreeNode
+	attr_accessor :parent, :children, :value
+
+	def initialize(value = nil)
+		@value = value
+		@parent = nil
+		@children = []
+	end
+
+	def add_child(child)
+		@children << child
+		child.parent = self
+	end
+
+	def remove_child! (child)
+		@children.delete(child)
+	end
+
+	def child_count
+		@children.count
+	end
+
+	def is_leaf?
+		@children.empty?
+	end
+
+	def is_root?
+		@parent.nil?
+	end
+
+	def descendents
+		@children.map { |child| [child, child.descendents] }.flatten
+	end
+
+end
